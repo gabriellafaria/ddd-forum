f657257ff444be51fbd450614ac6614e2e03ce20
Gabriella Faria <gabisouf@gmail.com>
2024-05-05 21:50:40 -0300

    feat: add findManyRecent and save function on repository

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
@@ -14,8 +15,21 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     return answer
   }
 
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((answer) => answer.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
+  }
+
   async create(answer: Answer) {
     this.items.push(answer)
+  }
+
+  async save(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+    this.items[itemIndex] = answer
   }
 
   async delete(answer: Answer) {
